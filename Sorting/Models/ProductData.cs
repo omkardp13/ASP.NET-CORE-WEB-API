5e72d75eb25fb6750bf8e4ff65db22ad5351d9c3
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-05 09:49:11 +0530

    Add files via upload

---
+++ Sorting/Models/ProductData.cs
@@ -0,0 +1,27 @@
+﻿// Data/ProductData.cs
+using DemoPractice.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ProductData
+{
+    private static List<Product> Products = new List<Product>
+    {
+        new Product { Id = 1, Name = "Product1", Price = 10.0m },
+        new Product { Id = 2, Name = "Product2", Price = 20.0m },
+        new Product { Id = 3, Name = "Product3", Price = 15.0m },
+    };
+
+    public static IEnumerable<Product> GetProducts(string sortBy)
+    {
+        switch (sortBy?.ToLower())
+        {
+            case "name":
+                return Products.OrderBy(p => p.Name);
+            case "price":
+                return Products.OrderBy(p => p.Price);
+            default:
+                return Products.OrderBy(p => p.Id);
+        }
+    }
+}
