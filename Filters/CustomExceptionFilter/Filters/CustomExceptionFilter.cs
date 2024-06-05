ff92c732656626d28a4a07a9241fc995275032aa
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-05 08:49:10 +0530

    Add files via upload

---
+++ Filters/CustomExceptionFilter/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,35 @@
+﻿// Filters/CustomExceptionFilter.cs
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace CustomExceptionFilterExample.Filters
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<CustomExceptionFilter> _logger;
+
+        public CustomExceptionFilter(ILogger<CustomExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception, "An unhandled exception occurred.");
+
+            var response = new
+            {
+                Message = "An error occurred while processing your request.",
+                Detail = context.Exception.Message
+            };
+
+            context.Result = new ObjectResult(response)
+            {
+                StatusCode = 500
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
