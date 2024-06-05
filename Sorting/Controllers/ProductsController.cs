c84aec85efb83d39a0c7ca5fab7add800577def7
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-05 09:48:27 +0530

    Add files via upload

---
+++ Sorting/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using DemoPractice.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoPractice.Controllers
+{
+
+    /*
+     * http://localhost:5000/api/products (default sorting by Id)
+     *  http://localhost:5000/api/products?sortBy=name (sorting by Name)
+     *  http://localhost:5000/api/products?sortBy=price (sorting by Price)
+     * 
+     * 
+     * 
+     */
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts([FromQuery] string sortBy)
+        {
+            var products = ProductData.GetProducts(sortBy);
+            return Ok(products);
+        }
+    }
+}
