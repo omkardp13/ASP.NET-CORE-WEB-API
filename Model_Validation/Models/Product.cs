a48081e5a41a005023c0ae08e3cb2206389e459b
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-05 09:36:52 +0530

    Add files via upload

---
+++ Model_Validation/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using CustomValidationExample.Validators;
+
+namespace CustomValidationExample.Models
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Range(0.01, 10000.00)]
+        public decimal Price { get; set; }
+
+        [ReleaseDateInFuture]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
