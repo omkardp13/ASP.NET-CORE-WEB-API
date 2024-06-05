456504be591f6f7046c01af043ede7199515f42d
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-05 09:45:45 +0530

    Add files via upload

---
+++ Filtering/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoPractice.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
