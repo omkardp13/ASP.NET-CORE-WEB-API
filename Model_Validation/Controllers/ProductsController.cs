b513ff6ae3a8c7053259f49deef77ede9986b64f
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-05 09:36:00 +0530

    Add files via upload

---
+++ Model_Validation/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CustomValidationExample.Models;
+
+namespace CustomValidationExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Normally, save the product to the database here.
+
+            return Ok(product);
+        }
+    }
+}
