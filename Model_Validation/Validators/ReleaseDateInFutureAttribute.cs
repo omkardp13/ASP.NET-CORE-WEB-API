050d25f1be29a9783a589f94fd2405364f990ce7
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-05 09:38:00 +0530

    Add files via upload

---
+++ Model_Validation/Validators/ReleaseDateInFutureAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+//create custom validation attribute
+namespace CustomValidationExample.Validators
+{
+    public class ReleaseDateInFutureAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is DateTime releaseDate)
+            {
+                if (releaseDate > DateTime.UtcNow)
+                {
+                    return ValidationResult.Success;
+                }
+                return new ValidationResult("Release date must be in the future.");
+            }
+            return new ValidationResult("Invalid release date.");
+        }
+    }
+}
