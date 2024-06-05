233b6b81ae6ee52e8610e9b1e25a3cac85096d47
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-05 08:51:45 +0530

    Add files via upload

---
+++ Filters/CustomExceptionFilter/Services/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using PracticeConcepts.Models;
+
+namespace PracticeConcepts.Services
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product GetById(int id);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(int id);
+    }
+}
---
+++ Filters/CustomExceptionFilter/Services/ProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using PracticeConcepts.Models;
+
+namespace PracticeConcepts.Services
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly List<Product> _products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product 1", Price = 10.00M },
+            new Product { Id = 2, Name = "Product 2", Price = 20.00M }
+        };
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public Product GetById(int id)
+        {
+            return _products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Update(Product product)
+        {
+            var existingProduct = GetById(product.Id);
+            if (existingProduct != null)
+            {
+                existingProduct.Name = product.Name;
+                existingProduct.Price = product.Price;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var product = GetById(id);
+            if (product != null)
+            {
+                _products.Remove(product);
+            }
+        }
+    }
+}
