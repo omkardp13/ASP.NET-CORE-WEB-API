710d6b1d8eab610f28941140fb50bb10a647cb57
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-05 08:45:41 +0530

    Add files via upload

---
+++ Filters/CustomExceptionFilter/Controllers/ProductsController.cs
@@ -0,0 +1,75 @@
+﻿// Controllers/ProductsController.cs
+using Microsoft.AspNetCore.Mvc;
+using PracticeConcepts.Models;
+using PracticeConcepts.Services;
+using System.Collections.Generic;
+
+namespace CustomExceptionFilterExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _productRepository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            var product = _productRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+        [HttpPost]
+        public ActionResult<Product> Post(Product product)
+        {
+            _productRepository.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingProduct = _productRepository.GetById(id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+
+            _productRepository.Update(product);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existingProduct = _productRepository.GetById(id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+
+            _productRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
