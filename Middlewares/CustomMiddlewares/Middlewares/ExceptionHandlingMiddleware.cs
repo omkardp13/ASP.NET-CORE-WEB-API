50fa8f4f37473d918c37f6ffe3d05883e94e76e6
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-06 20:42:23 +0530

    Add files via upload

---
+++ Middlewares/CustomMiddlewares/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unhandled exception has occurred.");
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var response = new
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = "An error occurred while processing your request.",
+            Detailed = exception.Message
+        };
+
+        var jsonResponse = JsonSerializer.Serialize(response);
+
+        return context.Response.WriteAsync(jsonResponse);
+    }
+}
---
+++ Middlewares/CustomMiddlewares/Middlewares/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+public class RequestResponseLoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<RequestResponseLoggingMiddleware> _logger;
+
+    public RequestResponseLoggingMiddleware(RequestDelegate next, ILogger<RequestResponseLoggingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        // Log the request path
+        _logger.LogInformation($"Request Path: {context.Request.Path}");
+
+        // Call the next middleware in the pipeline
+        await _next(context);
+
+        // Log the response status code
+        _logger.LogInformation($"Response Status Code: {context.Response.StatusCode}");
+    }
+}
