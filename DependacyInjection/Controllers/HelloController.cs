86d4568c2825c04f5b1fd0cfcafd6f58234e4b25
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-06 19:37:09 +0530

    Add files via upload

---
+++ DependacyInjection/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        private readonly IGreetingService _greetingService;
+
+        public HelloController(IGreetingService greetingService)
+        {
+            _greetingService = greetingService;
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult Get(string name)
+        {
+            var greeting = _greetingService.Greet(name);
+            return Ok(greeting);
+        }
+    }
+}
