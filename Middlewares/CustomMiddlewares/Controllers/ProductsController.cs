6ca4d6708de45740f92c182977d3258a3222a89b
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-06 20:41:35 +0530

    Add files via upload

---
+++ Middlewares/CustomMiddlewares/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PracticeConcepts.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+     
+        public ProductsController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            throw new Exception("Test exception");
+            // Simulated data retrieval
+            var products = new[] {
+                new { Id = 1, Name = "Product1", Price = 10.0 },
+                new { Id = 2, Name = "Product2", Price = 20.0 }
+            };
+            return Ok(products);
+        }
+    }
+}
