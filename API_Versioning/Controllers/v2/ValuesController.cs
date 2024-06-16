6b4d0f68ab36eaaf5588f28c3626081ccd9502f3
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-16 22:27:28 +0530

    Add files via upload

---
+++ API_Versioning/Controllers/v2/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logging.Controllers.V2
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { version = "v2", value = "Hello from API version 2.0" });
+        }
+    }
+}
