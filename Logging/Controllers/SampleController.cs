3eca40cd4507be83d3298b0e736762b41514c71f
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-16 20:42:49 +0530

    Add files via upload

---
+++ Logging/Controllers/SampleController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace Logging.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Log.Information("This is an information log");
+            Log.Warning("This is a warning log");
+
+            try
+            {
+                throw new Exception("This is a test exception");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occurred while processing the request");
+                return StatusCode(500, "Internal server error");
+            }
+
+            return Ok("Hello, world!");
+        }
+    }
+}
