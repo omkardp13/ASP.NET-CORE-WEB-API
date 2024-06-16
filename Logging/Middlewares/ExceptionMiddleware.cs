77504ffe38c424816543e5e735c272ec0d8d42d9
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-06-16 20:44:17 +0530

    Add files via upload

---
+++ Logging/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Logging.Models;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "An unhandled exception has occurred");
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        return context.Response.WriteAsync(new ErrorDetails
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = "Internal Server Error from the custom middleware."
+        }.ToString());
+    }
+}
