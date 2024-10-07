314e8ccf2a7b6a74103c2cbc43e75ed3c36a48d5
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2024-10-07 14:26:30 +0530

    Add files via upload

---
+++ AutoMapper/Helpers/UserProfile.cs
@@ -0,0 +1,21 @@
+﻿using Automapper.DTO_s;
+using Automapper.Models;
+
+
+namespace Automapper.Helpers
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            // Mapping User entity to UserDto
+            CreateMap<User, UserDto>()
+                .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"));
+
+            // Optionally, reverse mapping if needed
+            CreateMap<UserDto, User>()
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FullName.Split(' ')[0]))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.FullName.Split(' ')[1]));
+        }
+    }
+}
